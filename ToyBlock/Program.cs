d61fdd22a5257a34af4514171fb67b1eae5b7783
Viet Anh Tran <tranvietanh1996@gmail.com>
2020-10-16 20:59:13 +1300

    Initial set up

---
+++ ToyBlock/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ToyBlock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
